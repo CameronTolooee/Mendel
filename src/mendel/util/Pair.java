56a8c5aa0a2ae05887abaafd0c17f13a69c35cf3
Cameron Tolooee <Cameron.Tolooee@gmail.com>
2014-12-02 21:32:31 -0700

    Add toString() method

--- src/mendel/util/Pair.java
+++ src/mendel/util/Pair.java
@@ -31,10 +31,15 @@ public class Pair<A, B> {
     public A a;
     public B b;
 
-    public Pair() { }
+    public Pair() {
+    }
 
     public Pair(A a, B b) {
         this.a = a;
         this.b = b;
+    }
+
+    public String toString() {
+        return a + " " + b;
     }
 }
